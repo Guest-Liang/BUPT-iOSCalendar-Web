884831f9155535a3d0964ef542d359a228dbce2c
Guest-Liang <GuestLiang@outlook.com>
2023-04-19 22:36:58 +0800

    拉取html部分完成

---
+++ Constant.py
@@ -0,0 +1,84 @@
+#个人信息Personal Information
+SchoolID='2021212702'
+Password_jwgl='1023BUPTjwgl'
+
+
+#用于登录的URL以及UA
+USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.0.0'
+LOGIN_URL='https://jwgl.bupt.edu.cn/jsxsd/'
+GET_API='https://jwgl.bupt.edu.cn/jsxsd/xskb/xskb_list.do'
+POST_URL='https://jwgl.bupt.edu.cn/jsxsd/xk/LoginToXk'
+KBJCMSID={
+    '0':'9475847A3F3033D1E05377B5030AA94D', #默认节次模式
+    '1':'857D7D63586B4AF4B4C59B1DFEEB56E4' #海南校区课表时间
+} #由[https://jwgl.bupt.edu.cn/jsxsd/framework/xsMain.jsp]的html代码得到的'课表节次模式id'
+
+KBLB={
+    '0':'kckb', #课程课表
+    '1':'tzdkb' #通知单课表
+} #由[https://jwgl.bupt.edu.cn/jsxsd/framework/xsMain.jsp]的html代码得到的'课表类别'
+
+#教务系统加密算法，由[https://jwgl.bupt.edu.cn/jsxsd/framework/xsMain.jsp]得到
+KEYSTRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+CONWORK_JS='''
+eval(function (p, a, c, k, e, d)
+{
+    e = function (c){return (c < a ? "": e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))};
+    if (!''.replace(/^/, String))
+    {
+        while (c--) d[e(c)] = k[c] || e(c);
+        k = [function (e){return d[e]}];
+        e = function() {return '\\w+'};
+        c = 1;
+    };
+    while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+    return p;
+} ('b 9="o+/=";p q(a){b e="";b 8,5,7="";b f,g,c,1="";b i=0;m{8=a.h(i++);5=a.h(i++);7=a.h(i++);f=8>>2;g=((8&3)<<4)|(5>>4);c=((5&s)<<2)|(7>>6);1=7&t;k(j(5)){c=1=l}v k(j(7)){1=l}e=e+9.d(f)+9.d(g)+9.d(c)+9.d(1);8=5=7="";f=g=c=1=""}u(i<a.n);r e}', 32, 32, '|enc4||||chr2||chr3|chr1|keyStr|input|var|enc3|charAt|output|enc1|enc2|charCodeAt||isNaN|if|64|do|length|ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789|function|encodeInp|return|15|63|while|else'.split('|'), 0, {}));
+'''
+
+#很明显是eval加密，解压一下就得到原始js代码
+CONWORK_JS_DECODED='''
+var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+function encodeInp(input) {
+	var output = "";
+	var chr1, chr2, chr3 = "";
+	var enc1, enc2, enc3, enc4 = "";
+	var i = 0;
+	do {
+		chr1 = input.charCodeAt(i++);
+		chr2 = input.charCodeAt(i++);
+		chr3 = input.charCodeAt(i++);
+		enc1 = chr1 >> 2;
+		enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+		enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+		enc4 = chr3 & 63;
+		if (isNaN(chr2)) {
+			enc3 = enc4 = 64
+		} else if (isNaN(chr3)) {
+			enc4 = 64
+		}
+		output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
+		chr1 = chr2 = chr3 = "";
+		enc1 = enc2 = enc3 = enc4 = ""
+	} while (i < input.length);
+	return output
+}
+'''
+
+
+#referer_get_api='https://jwgl.bupt.edu.cn/jsxsd/framework/xsMain.jsp'
+#userAccount_xpath='/html/body/div/div[2]/div[3]/form/ul[1]/li[2]/input' #由LOGIN_API的html代码得到
+#userPassword_xpath='/html/body/div/div[2]/div[3]/form/ul[1]/li[3]/input' #由LOGIN_API的html代码得到
+
+class HEADERS:
+#    REFERER_LOGIN_API = 'https://app.bupt.edu.cn/uc/wap/login'
+#    UA = ('Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) '
+#          'Mobile/15E148 MicroMessenger/7.0.11(0x17000b21) NetType/4G Language/zh_CN')
+#    ACCEPT_JSON = 'application/json'
+    ACCEPT_HTML='text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+#    REQUEST_WITH_XHR = 'XMLHttpRequest'
+    ACCEPT_LANG='zh-cn'
+#    CONTENT_TYPE_UTF8 = 'application/x-www-form-urlencoded; charset=UTF-8'
+#    CONTENT_TYPE_FORM = 'application/x-www-form-urlencoded'
+    USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.0.0'
---
+++ Main.py
@@ -0,0 +1,67 @@
+import requests
+import icalendar
+import re
+import datetime
+import execjs
+from Constant import *
+
+def GetXNXQ(): #获取学年学期，形如'2022-2023-2'，2-7月为第二学期，8-次年1月为第一学期
+    CurrentYear=datetime.datetime.now().year
+    CurrentMonth=datetime.datetime.now().month
+    if CurrentMonth>=2 and CurrentMonth<=7:
+        XNXQ=str(CurrentYear-1)+'-'+str(CurrentYear)+'-2'
+    else:
+        XNXQ=str(CurrentYear)+'-'+str(CurrentYear+1)+'-1'
+    return XNXQ
+
+#直接编译运行js文件，获取加密后的学号和密码
+ctx=execjs.compile(CONWORK_JS_DECODED)
+Encoded=str(ctx.call("encodeInp", SchoolID))+"%%%"+str(ctx.call("encodeInp", Password_jwgl)) #加密后的学号和密码
+
+Xueqi=GetXNXQ(); #获取学年学期，形如'2022-2023-2'
+#Xueqi='' #可以手动输入学年学期，形如'2022-2023-2'
+UserData={'userAccount':SchoolID,'userPassword':Password_jwgl,'encoded':Encoded} #用户数据，保存在Constant.py中，根据需要修改
+
+#登录教务系统
+print('登录北邮教务管理系统')
+Session=requests.Session()
+Login=Session.get(url=LOGIN_URL,headers={'User-Agent':USER_AGENT})
+Cookies1=Login.cookies.items() #处理cookies
+Cookie=''
+for Name, Value in Cookies1:
+    Cookie+=f'{Name}={Value};'
+
+#设置headers，先post用户数据
+Headers_UserData={
+    'Host':'jwgl.bupt.edu.cn',
+    'Referer':'https://jwgl.bupt.edu.cn/jsxsd/xk/LoginToXk?method=exit&tktime=1631723647000',
+    'User-Agent':USER_AGENT,
+    'cookie':Cookie
+}
+Session.post(url=POST_URL,data=UserData,headers=Headers_UserData) #由LOGIN_URL的html决定是post方法，action='/jsxsd/xk/LoginToXk'
+Cookies2=Login.cookies.items() #登录后cookie可能变了，重新处理一下
+Cookie_Logged_In=''
+for Name, Value in Cookies2:
+    Cookie_Logged_In+='{0}={1}; '.format(Name,Value)
+
+#设置headers
+Headers={
+    'User-Agent':USER_AGENT,
+    'cookie':Cookie_Logged_In
+}
+#周次（默认全部），学年学期（默认当前学期），课表节次id，课表类别
+Info={'zc':'','xnxq01id':Xueqi,'kbjcmsid':KBJCMSID['0'],'kblb':KBLB['0']}
+Url='https://jwgl.bupt.edu.cn/jsxsd/xskb/xskb_list.do'
+Data=requests.post(url=Url,headers=Headers,data=Info) #获取课表html
+if "请先登录系统" in Data.text:
+    print("登录失败，请检查学号和密码以及网络连接，并重新运行程序")
+    exit()
+
+#将.do写入本地html文件
+print("正在写入本地html")
+try:
+    with open(f'./{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")}.html', 'w', encoding='utf-8') as file:
+        file.write(Data.text)
+        print(f'[success]')
+except Exception:
+    print("生成文件失败")
